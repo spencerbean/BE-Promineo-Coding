e6bd35ab9e2c6c6a28194d814173043cee212117
spencerbean <beanfam15@gmail.com>
2024-03-01 17:47:01 -0700

    .sql file

--- Logger.java
+++
@@ -1,8 +0,0 @@
-package week05CodingHw;
-
-public interface Logger { // Logger interface
-	
-	public void log(String log); // abstract method
-	public void error(String error); // abstract method
-	
-}
